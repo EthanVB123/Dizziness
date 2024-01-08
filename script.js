688b74d3f9bf149595006108dcdd2ec35a8ce7c5
EthanVB123 <ethanvanbruchem@gmail.com>
2024-01-08 12:57:40 +0800

    Substantial progress made on the text file rendering correctly

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 console.log("Hi");
 
 function submitForm() {
+    var outputString = "";
+    // Name
     var name = document.getElementById('name').value;
-    console.log(name);
-    downloadResults("results.txt", "Your name is "+name);
+    outputString += "Your name is " + name + "\n";
+    // Age
+    var age = document.getElementById('age').value;
+    outputString += "You are " + age + " years old\n";
+    // Main Problem
+    outputString += "Main Problem(s):\n"
+    var dizziness = document.getElementById('dizziness').checked;
+    console.log(dizziness);
+    outputString += dizziness ? "Dizziness\n" : "";
+    var imbalance = document.getElementById('imbalance').checked;
+    console.log(imbalance);
+    outputString += imbalance ? "Imbalance\n" : "";
+    
+    var earSymptoms = document.getElementById('earSymptoms').checked;
+    outputString += earSymptoms ? "Ear Symptoms\n": "";
+    outputString += "\n";
+    downloadResults("results.txt", outputString);
 }
 function downloadResults(filename, text) {
     /*var filename = "results.txt";
