607ecf86dff051dbfc0e033691958529f6179af0
EthanVB123 <ethanvanbruchem@gmail.com>
2024-01-08 12:34:27 +0800

    Found a workaround which should make my life a lot easier! Now there can be a text file taken from the results of the form.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Hi");
+
+function submitForm() {
+    var name = document.getElementById('name').value;
+    console.log(name);
+    downloadResults("results.txt", "Your name is "+name);
+}
+function downloadResults(filename, text) {
+    /*var filename = "results.txt";
+    var text = "This is a preliminary test."*/
+
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    
+    element.click();
+    
+    document.body.removeChild(element);
+  }
